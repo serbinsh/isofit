e2af8f7e4bf9dee54b5f298aba98be2ee6f30a19
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2020-07-10 14:02:37 -0400

    Flesh out endmember analysis

--- examples/r-interface/examples/above-endmember-analysis.R
+++ examples/r-interface/examples/above-endmember-analysis.R
@@ -1,4 +1,10 @@
 library(here)
+library(dplyr)
+library(tidyr)
+library(purrr)
+library(ggplot2)
+library(fst)  # For efficient output storage
+
 source(here("examples", "r-interface", "config.R"))
 source(here("examples", "r-interface", "functions.R"))
 
@@ -11,20 +17,56 @@ lrt <- read_libradtran_template(here(
 
 endmembers <- read.csv(file.path(cwd, "endmembers.csv"))
 wavelengths <- endmembers$wavelength
-endmember_mat <- as.matrix(endmembers[,-1])
+endmember_mat <- as.matrix(endmembers[, -1])
 
-outlist <- list()
-for (i in seq_len(nrow(atmosphere_df))) {
-  tag <- paste0("atmosphere", i)
-  outlist[[tag]] <- ht_workflow(
-    endmember_mat,
-    atmosphere_df[i, "H2O"],
-    atmosphere_df[i, "AOD"],
-    wavelengths,
-    lrt,
-    outdir
+ht_matrix <- crossing(
+  nesting(true_h2o = atmosphere_df$H2O, true_aot = atmosphere_df$AOD),
+  noisefile = list.files(file.path(cwd, "noisefiles"), full.names = TRUE)
+)
+
+run_ht <- function(true_h2o, true_aot, noisefile) {
+  ht_workflow(
+    endmember_mat, true_h2o, true_aot, wavelengths, lrt, outdir,
+    instrument_configs = list(parametric_noise_file = noisefile,
+                              integrations = 1)
   )
 }
 
-results <- do.call(cbind, lapply(outlist, "[[", "reflectance"))
-matplot(wavelengths, results, type = "l")
+ht_matrix$results <- pmap(ht_matrix, run_ht)
+
+htm2 <- ht_matrix %>%
+  mutate(
+    true_refl = list(as_tibble(endmember_mat)),
+    est_refl = map(results, "reflectance") %>%
+      map(`colnames<-`, colnames(endmember_mat)) %>%
+      map(tibble::as_tibble),
+    wavelength = list(wavelengths)
+  ) %>%
+  dplyr::select(-results) %>%
+  unnest(c(est_refl, true_refl, wavelength), names_sep = "|") %>%
+  rename(wavelength = "wavelength|wavelength")
+
+results_long <- htm2 %>%
+  pivot_longer(
+    -c(true_h2o, true_aot, noisefile, wavelength),
+    names_to = c("source", "endmember"),
+    names_sep = "\\|"
+  ) %>%
+  separate(endmember, c("type", "id"), sep = "\\.")
+
+results2 <- results_long %>%
+  pivot_wider(
+    names_from = "source",
+    values_from = "value"
+  )
+
+write_fst(results2, file.path(cwd, "results.fst"))
+
+# Some basic summary stats
+summaries <- results2 %>%
+  group_by(true_h2o, true_aot, noise = basename(noisefile), type) %>%
+  summarize(rmse = sqrt(mean((true_refl - est_refl)^2, na.rm = TRUE)))
+
+ggplot(summaries) +
+  aes(x = noise, y = rmse, fill = type) +
+  geom_boxplot()
