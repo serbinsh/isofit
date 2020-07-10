7ed15d2f05461badc12596fa2f8faf08f193dec4
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2020-07-10 13:09:55 -0400

    Self-contained endmember analysis

--- examples/r-interface/examples/above-endmember-analysis.R
+++ examples/r-interface/examples/above-endmember-analysis.R
@@ -3,38 +3,27 @@ library(raster)
 source(here("examples", "r-interface", "config.R"))
 source(here("examples", "r-interface", "functions.R"))
 
-envi_dir <- file.path("~", "projects", "sbg-uncertainty",
-                      "sbg-prosail-workflow", "data",
-                      "outputs", "envi-endmembers")
-
 cwd <- here("examples", "r-interface", "examples")
 atmosphere_df <- read.csv(file.path(cwd, "atmospheres.csv"))
 outdir <- here("examples", "r-interface", "output")
-
-endmembers <- c("Coastal_water", "Inland_water", "Mineral",
-                "Snow", "Vegetation")
-
-endmember_files <- file.path(envi_dir, endmembers)
-wavelengths <- as.numeric(readLines(file.path(cwd, "above-wavelengths.txt")))
 lrt <- read_libradtran_template(here(
   "examples", "r-interface", "lrt_template.inp"
 ))
 
-datlist <- lapply(endmember_files, function(x) t(brick(x)[]))
-dat <- do.call(cbind, datlist)
-dat[dat < 0] <- 0
+endmembers <- read.csv(file.path(cwd, "endmembers.csv"))
+wavelengths <- endmembers$wavelength
+endmember_mat <- as.matrix(endmembers[,-1])
 
 outlist <- list()
 for (i in seq_len(nrow(atmosphere_df))) {
   tag <- paste0("atmosphere", i)
   outlist[[tag]] <- ht_workflow(
-      dat,
-      atmosphere_df[i, "H2O"],
-      atmosphere_df[i, "AOD"],
-      wavelengths,
-      lrt,
-      libradtran_basedir = LIBRADTRAN_DIR,
-      outdir = outdir
+    endmember_mat,
+    atmosphere_df[i, "H2O"],
+    atmosphere_df[i, "AOD"],
+    wavelengths,
+    lrt,
+    outdir
   )
 }
 
